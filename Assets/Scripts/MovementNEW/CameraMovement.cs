7ca2e42f2dcb7ff5492a2a9ef053d55e1c170ec8
Alex <ovcinikov@gmail.com>
2020-05-15 14:40:49 +0100

    Redone camera rotation, code cleanup
    Camera now rotates using Quaternions, cleaned up code.

--- Assets/Scripts/MovementNEW/CameraMovement.cs
+++ Assets/Scripts/MovementNEW/CameraMovement.cs
@@ -4,26 +4,30 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    private bool _startedRotation;
     private Vector3 _lookPoint;
     private Vector3 _cameraF;
     private Vector3 _cameraR;
-    private Transform cameraPivot => currentCamera.transform.parent;
-    private bool _startedRotation;
+    private Vector3 _goalRotateEuler;
+    private Quaternion _goalRotateQuat;
     [SerializeField] private float rotateSpeed;
     [SerializeField] private GameObject currentCamera;
     [SerializeField] private GameObject[] listOfCameras;
     public Vector3 CameraF => _cameraF;
     public Vector3 CameraR => _cameraR;
+    private Transform CameraPivot => currentCamera.transform.parent;
+    private Transform CameraTransform => currentCamera.transform;
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < listOfCameras.Length; i++)
+        foreach (var cam in listOfCameras)
         {
-            if(listOfCameras[i] == currentCamera) continue;
-            listOfCameras[i].SetActive(false);
+            if(cam == currentCamera) continue;
+            cam.SetActive(false);
         }
-        _lookPoint = cameraPivot.position;
-        UpdateCamera();
+        _lookPoint = CameraPivot.position;
+        UpdateCameraView();
+        UpdateMovementDirection();
     }
 
     // Update is called once per frame
@@ -42,49 +46,49 @@ public class CameraMovement : MonoBehaviour
     
     private void RotateCamera(int angle)
     {
-        Vector3 goalEuler = new Vector3(0, cameraPivot.transform.eulerAngles.y + angle, 0);
-        //cameraPivot.eulerAngles = goalEuler; //Line of code to bring back snappy camera rotation
+        _goalRotateEuler = new Vector3(0, CameraPivot.transform.eulerAngles.y + angle, 0);
         _startedRotation = true;
-        StartCoroutine(SmoothRotate(goalEuler));
+        StartCoroutine(SmoothRotate(_goalRotateEuler));
     }
     
-    private void UpdateCamera()
+    private void UpdateCameraView()
     {
-        Transform camT = currentCamera.transform;
-        camT.LookAt(_lookPoint);
-        _cameraF = new Vector3(camT.forward.x, 0f, camT.forward.z).normalized;
-        _cameraR = new Vector3(camT.right.x, 0f, camT.right.z).normalized;
+        CameraTransform.LookAt(_lookPoint);
+    }
+    
+    private void UpdateMovementDirection()
+    {
+        _cameraF = new Vector3(CameraTransform.forward.x, 0f, CameraTransform.forward.z).normalized;
+        _cameraR = new Vector3(CameraTransform.right.x, 0f, CameraTransform.right.z).normalized;
     }
 
     public void ChangeRoom(int room)
     {
-        var angle = cameraPivot.rotation.eulerAngles;
+        var angle = CameraPivot.rotation.eulerAngles;
         currentCamera.SetActive(false);
         currentCamera = listOfCameras[room];
         currentCamera.SetActive(true);
         _lookPoint = currentCamera.transform.parent.position;
-        if (cameraPivot.rotation.eulerAngles != angle)
+        if (CameraPivot.rotation.eulerAngles != angle)
         {
-            cameraPivot.eulerAngles = angle;
+            CameraPivot.eulerAngles = angle;
         }
-        UpdateCamera();
+        UpdateCameraView();
     }
 
     IEnumerator SmoothRotate(Vector3 goalEuler)
     {
         do
         {
-            float angle = Mathf.LerpAngle(cameraPivot.eulerAngles.y, goalEuler.y, rotateSpeed * Time.deltaTime);
-            cameraPivot.eulerAngles = new Vector3(0,angle,0);
-            var distance = Vector3.Distance(cameraPivot.eulerAngles, goalEuler);
-            if (distance < 0.1f || distance > 360 && distance < 361) //Needs redoing potentially
+            _goalRotateQuat = Quaternion.Euler(goalEuler);
+            CameraPivot.rotation = Quaternion.RotateTowards(CameraPivot.rotation, _goalRotateQuat, rotateSpeed);
+            if (Quaternion.Angle(CameraPivot.rotation, _goalRotateQuat) < 0.1f)
             {
                 _startedRotation = false;
-                var euler = new Vector3(0, Mathf.Round(cameraPivot.eulerAngles.y),0);
-                cameraPivot.eulerAngles = euler;
             }
-            UpdateCamera();
+            UpdateCameraView();
             yield return null;
         } while (_startedRotation);
+        UpdateMovementDirection();
     }
 }
--- Assets/Scripts/MovementNEW/PlayerMovement.cs
+++ Assets/Scripts/MovementNEW/PlayerMovement.cs
@@ -11,14 +11,11 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     private Ray _ray;
     private RaycastHit _hit;
+    private Vector3 _playerInput;
+    private Quaternion _playerRotation;
     [SerializeField] private Rigidbody playerRb;
     [SerializeField] private CameraMovement cameraScript;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
@@ -27,14 +24,12 @@ public class PlayerMovement : MonoBehaviour
 
     private void MovePlayer()
     {
-        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        input = input.normalized;
-        if (input != Vector3.zero)
-        {
-            Quaternion rotation = Quaternion.LookRotation(input);
-            playerRb.velocity = (cameraScript.CameraF * input.z + cameraScript.CameraR * input.x) * speed;
-            playerRb.rotation = rotation;
-        }
+        _playerInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        if (_playerInput == Vector3.zero) return;
+        _playerInput = _playerInput.normalized;
+        _playerRotation = Quaternion.LookRotation(_playerInput);
+        playerRb.velocity = (cameraScript.CameraF * _playerInput.z + cameraScript.CameraR * _playerInput.x) * speed;
+        playerRb.rotation = _playerRotation;
     }
 
     private void OnCollisionEnter(Collision other)
