239933c64f6dead80c0ffcaf67cf4eac52c4c689
Ian Barrow <barrowian1991@Gmail.com>
2020-08-12 14:33:31 +0100

    Updated to latest version of dialogue package

--- Assets/ScriptableDialogueSystem/Editor/DialogueTypes/Message.cs
+++ Assets/ScriptableDialogueSystem/Editor/DialogueTypes/Message.cs
@@ -12,11 +12,30 @@ namespace ScriptableDialogueSystem.Editor.DialogueTypes
         [SerializeField] private string _npcMood;
         [SerializeField] private List<Response> _responses;
         [SerializeField] private int _nextMessage;
-        public String NpcName => _npcName;
-        public string NpcMood => _npcMood;
-        public string MessageText => _messageText;
+        public String NpcName
+        {
+            get => _npcName;
+            set => _npcName = value;
+        }
+
+        public string NpcMood
+        {
+            get => _npcMood;
+            set => _npcMood = value;
+        }
+
+        public string MessageText
+        {
+            get => _messageText;
+            set => _messageText = value;
+        }
+
         public List<Response> Responses => _responses;
 
-        public int NextMessage => _nextMessage;
+        public int NextMessage
+        {
+            get => _nextMessage;
+            set => _nextMessage = value;
+        }
     }
 }
