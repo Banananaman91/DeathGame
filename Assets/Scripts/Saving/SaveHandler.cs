49e0dfc030caf46ac4cc03cde3151883490441f5
Alex <ovcinikov@gmail.com>
2020-08-27 16:29:17 +0100

    Save/load
    Added simple saving/loading. Currently only for player position and rotation

---
+++ Assets/Scripts/Saving/SaveHandler.cs
@@ -0,0 +1,65 @@
+﻿using MovementNEW;
+using UnityEngine;
+
+namespace Saving
+{
+    public class SaveHandler : MonoBehaviour
+    {
+        private static PlayerMovement PlayerMovement => FindObjectOfType<PlayerMovement>();
+        private Vector3 PlayerPosition
+        {
+            get => PlayerMovement.transform.position;
+            set => PlayerMovement.transform.position = value;
+        }
+        private static Quaternion PlayerRotation
+        {
+            get => PlayerMovement.transform.rotation;
+            set => PlayerMovement.transform.rotation = value;
+        }
+        
+        private void Awake()
+        {
+            SaveSystem.Initialize();
+            Load();
+        }
+
+        public void Save()
+        {
+            var playerPos = PlayerPosition;
+            var playerRot = PlayerRotation;
+            var saveData = new SaveData
+            {
+                playerPosition = playerPos,
+                playerRotation = playerRot
+            };
+            var json = JsonUtility.ToJson(saveData);
+            SaveSystem.Save(json);
+        }
+        
+        public void Load()
+        {
+            var saveString = SaveSystem.Load();
+            if (saveString != null)
+            {
+                var saveData = JsonUtility.FromJson<SaveData>(saveString);
+                PlayerPosition = saveData.playerPosition;
+                PlayerRotation = saveData.playerRotation;
+            }
+            else
+            {
+                Debug.Log("No save to load");
+            }
+        }
+
+        public void DeleteSave()
+        {
+            SaveSystem.Delete();
+        }
+        
+        private class SaveData
+        {
+            public Vector3 playerPosition;
+            public Quaternion playerRotation;
+        }
+    }
+}
---
+++ Assets/Scripts/Saving/SaveSystem.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace Saving
+{
+    public static class SaveSystem
+    {
+        private static readonly string SaveFolder = Application.dataPath + "/Saves";
+
+        public static void Initialize()
+        {
+            if (!Directory.Exists(SaveFolder))
+            {
+                Directory.CreateDirectory(SaveFolder);
+            }
+        }
+
+        public static void Save(string saveString)
+        {
+            File.WriteAllText(SaveFolder + "/playerinf.txt", saveString);
+        }
+
+        public static string Load()
+        {
+            if (File.Exists(SaveFolder + "/playerinf.txt"))
+            {
+                var data = File.ReadAllText(SaveFolder + "/playerinf.txt");
+                return data;
+            }
+            return null;
+        }
+
+        public static void Delete()
+        {
+            if (File.Exists(SaveFolder + "/playerinf.txt"))
+            {
+                File.Delete(SaveFolder + "/playerinf.txt");
+            }
+        }
+    }
+}
