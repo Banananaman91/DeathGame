23d07a8f3cca2871ff7f936b66d21a083ed6fe69
Alex <ovcinikov@gmail.com>
2020-09-08 23:04:24 +0100

    Redone inventory slots creation and allocation

--- Assets/Scripts/InventoryScripts/InventoryScript.cs
+++ Assets/Scripts/InventoryScripts/InventoryScript.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using Boo.Lang;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,30 +6,19 @@ namespace InventoryScripts
 {
     public class InventoryScript : MonoBehaviour {
 
-        [SerializeField] private Button[] _itemPlace;                       //creates an array of Buttons
-        [SerializeField] private GameObject[] _items;                       //creates an array of GameObjects
-        [SerializeField] private ManagerScript _manager;                    //reference to ManagerScript
-        public GameObject[] Items => _items;
-
-        private void Awake()
+        [SerializeField] private GameObject _buttonPrefab;
+        [SerializeField] private ManagerScript _manager;
+        [SerializeField] private Transform _parentObject;
+        private readonly List<GameObject> _items = new List<GameObject>();
+        public List<GameObject> Items => _items;
+        
+        public void AddItem(ItemPickUp newItem)
         {
-            for (int i = 0; i < _items.Length; i++)              //loop which goes through all the items GameObjects                                           
-            {
-                _itemPlace[i].gameObject.SetActive(false);       //turns off buttons in inventory at the start of the game
-            }
-        }
-
-        public void AddItem(ItemPickUp newItem)                                                           //function that adds item into inventory (requires a GameObject)
-        {
-           for(var i = 0; i <= _itemPlace.Length; i++)
-           {
-               if (_items[i] != null) continue;
-               _items[i] = newItem.gameObject;
-               _itemPlace[i].gameObject.SetActive(true);
-               _itemPlace[i].image.sprite = newItem.ObjectSprite;
-               _itemPlace[i].onClick.AddListener(() => _manager.LoadItemInformation(i));
-               return;
-           }
+            var invSlot = Instantiate(_buttonPrefab, _parentObject);
+            _items.Add(newItem.gameObject);
+            var button = invSlot.GetComponentInChildren<Button>();
+            button.image.sprite = newItem.ObjectSprite;
+            //button.onClick.AddListener(() => _manager.LoadItemInformation(newItem)); //Make it so item information is loaded using ItemPickUp
         }
     }
 }
