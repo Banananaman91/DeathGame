60a6e293e599701dbed81107fd813d7e246f0e51
Ian <barrowian1991@gmail.com>
2020-09-14 13:26:55 +0100

    Set up introduction of game

--- Assets/Scripts/Interaction/Introduction.cs
+++ Assets/Scripts/Interaction/Introduction.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MovementNEW;
 using ScriptableDialogueSystem.Editor.DialogueTypes;
 using UnityEngine;
 
@@ -6,9 +7,11 @@ namespace Interaction
 {
     public class Introduction : DialogueObject
     {
-        private void OnCollisionEnter(Collision other)
+        private void OnTriggerEnter(Collider other)
         {
-            _myEvent[0].Invoke();
+            if (!other.gameObject.GetComponent<PlayerMovement>()) return;
+            _pageRender.AssignResponseObject(this);
+            _pageRender.PlayDialogue(_myDialogue);
         }
     }
 }
