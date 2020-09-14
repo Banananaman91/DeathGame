742105e064326da0557847e1f5f932c522bd0da0
Ian <barrowian1991@gmail.com>
2020-09-14 14:26:00 +0100

    Added temporary floor switcher to stairs to allow transition between

--- Assets/Scripts/Movement/Floortransition.cs
+++ Assets/Scripts/Movement/Floortransition.cs
@@ -1,14 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Floortransition : MonoBehaviour {
-    [SerializeField] private GameObject _endPoint; //FIXING UR SHIT DAWG I GOT YOU
-    [SerializeField] private GameObject _player;
+public class Floortransition : MonoBehaviour
+{
+    [SerializeField] private GameObject _otherFloor;
+    [SerializeField] private bool _onOrOff;
 
-    //modified this
-    public void MovePlayerToEndpoint()
+    private void OnCollisionStay(Collision other)
     {
-        _player.transform.position = _endPoint.transform.position;            //teleport player
+        _otherFloor.SetActive(_onOrOff);
     }
 }
